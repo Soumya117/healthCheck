6ba0b7bca1b36a7847016de9b3d78ad987e879f0
Soumya <soumyaupadhyay.6@gmail.com>
2023-07-25 10:20:23 +0200

    Add the interface file

---
+++ healthCheck/interfaces/IMeasurement.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace healthCheck.interfaces
+{
+    public interface IMeasurement
+    {
+          public List<ScoreMapping> getScoreMapping() => throw new NotImplementedException() ;
+          int getScore(int value) => throw new NotImplementedException() ;
+
+          private static int getMinimumValidValue(List<ScoreMapping> scoreMappingList)
+          {
+              return scoreMappingList.OrderByDescending(i => i.MinValue).ToList()[0].MinValue;
+          }
+          private static int getMaximumValidValue(List<ScoreMapping> scoreMappingList)
+          {
+              return scoreMappingList.OrderByDescending(i => i.MaxValue).ToList()[0].MaxValue;
+          }
+
+          public static int calculateScoreFromMapping(int value, List<ScoreMapping> scoreMappingList, string measurementType)
+          {
+              foreach(ScoreMapping scoreMap in scoreMappingList)
+              {
+                 if (value > scoreMap.MinValue  && value <= scoreMap.MaxValue)
+                  {
+                    return scoreMap.Score;
+                  }
+              }
+              string errorMessage = string.Format("Value {0} for {1} is invalid. Please enter a value between {2} and {3}.",
+                   value, measurementType, getMinimumValidValue(scoreMappingList), getMaximumValidValue(scoreMappingList)
+              );
+              throw new InvalidValueException(errorMessage);
+          }
+          }
+}
+
+public interface MeasurementScoreMapping
+{
+    public List<ScoreMapping> getScoreMapping() => throw new NotImplementedException() ;
+    int getScore(int value) => throw new NotImplementedException() ;
+
+    private static int getMinimumValidValue(List<ScoreMapping> scoreMappingList)
+    {
+        return scoreMappingList.OrderByDescending(i => i.MinValue).ToList()[0].MinValue;
+    }
+    private static int getMaximumValidValue(List<ScoreMapping> scoreMappingList)
+    {
+        return scoreMappingList.OrderByDescending(i => i.MaxValue).ToList()[0].MaxValue;
+    }
+
+    public static int calculateScoreFromMapping(int value, List<ScoreMapping> scoreMappingList, string measurementType)
+    {
+        foreach(ScoreMapping scoreMap in scoreMappingList)
+        {
+           if (value > scoreMap.MinValue  && value <= scoreMap.MaxValue)
+            {
+              return scoreMap.Score;
+            }
+        }
+        string errorMessage = string.Format("Value {0} for {1} is invalid. Please enter a value between {2} and {3}.",
+             value, measurementType, getMinimumValidValue(scoreMappingList), getMaximumValidValue(scoreMappingList)
+        );
+        throw new InvalidValueException(errorMessage);
+    }
+}
