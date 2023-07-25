90137927766deda4c56c7f390b48a6d2af8f8be5
Soumya <soumyaupadhyay.6@gmail.com>
2023-07-25 13:37:36 +0200

    Fix getting minimum and maximum value logic

--- healthCheck/interfaces/IMeasurement.cs
+++ healthCheck/interfaces/IMeasurement.cs
@@ -7,61 +7,31 @@ namespace healthCheck.interfaces
 {
     public interface IMeasurement
     {
-          public List<ScoreMapping> getScoreMapping() => throw new NotImplementedException() ;
-          int getScore(int value) => throw new NotImplementedException() ;
+        public List<ScoreMapping> getScoreMapping() => throw new NotImplementedException() ;
+        int getScore(int value) => throw new NotImplementedException() ;
 
-          private static int getMinimumValidValue(List<ScoreMapping> scoreMappingList)
-          {
-              return scoreMappingList.OrderByDescending(i => i.MinValue).ToList()[0].MinValue;
-          }
-          private static int getMaximumValidValue(List<ScoreMapping> scoreMappingList)
-          {
-              return scoreMappingList.OrderByDescending(i => i.MaxValue).ToList()[0].MaxValue;
-          }
-
-          public static int calculateScoreFromMapping(int value, List<ScoreMapping> scoreMappingList, string measurementType)
-          {
-              foreach(ScoreMapping scoreMap in scoreMappingList)
-              {
-                 if (value > scoreMap.MinValue  && value <= scoreMap.MaxValue)
-                  {
-                    return scoreMap.Score;
-                  }
-              }
-              string errorMessage = string.Format("Value {0} for {1} is invalid. Please enter a value between {2} and {3}.",
-                   value, measurementType, getMinimumValidValue(scoreMappingList), getMaximumValidValue(scoreMappingList)
-              );
-              throw new InvalidValueException(errorMessage);
-          }
-          }
-}
-
-public interface MeasurementScoreMapping
-{
-    public List<ScoreMapping> getScoreMapping() => throw new NotImplementedException() ;
-    int getScore(int value) => throw new NotImplementedException() ;
-
-    private static int getMinimumValidValue(List<ScoreMapping> scoreMappingList)
-    {
-        return scoreMappingList.OrderByDescending(i => i.MinValue).ToList()[0].MinValue;
-    }
-    private static int getMaximumValidValue(List<ScoreMapping> scoreMappingList)
-    {
-        return scoreMappingList.OrderByDescending(i => i.MaxValue).ToList()[0].MaxValue;
-    }
-
-    public static int calculateScoreFromMapping(int value, List<ScoreMapping> scoreMappingList, string measurementType)
-    {
-        foreach(ScoreMapping scoreMap in scoreMappingList)
+        private static int getMinimumValidValue(List<ScoreMapping> scoreMappingList)
         {
-           if (value > scoreMap.MinValue  && value <= scoreMap.MaxValue)
-            {
-              return scoreMap.Score;
-            }
+            return scoreMappingList.OrderByDescending(i => -i.MinValue).ToList()[0].MinValue + 1;
         }
-        string errorMessage = string.Format("Value {0} for {1} is invalid. Please enter a value between {2} and {3}.",
-             value, measurementType, getMinimumValidValue(scoreMappingList), getMaximumValidValue(scoreMappingList)
-        );
-        throw new InvalidValueException(errorMessage);
+        private static int getMaximumValidValue(List<ScoreMapping> scoreMappingList)
+        {
+            return scoreMappingList.OrderByDescending(i => i.MaxValue).ToList()[0].MaxValue;
+        }
+
+        public static int calculateScoreFromMapping(int value, List<ScoreMapping> scoreMappingList, string measurementType)
+        {
+            foreach(ScoreMapping scoreMap in scoreMappingList)
+            {
+               if (value > scoreMap.MinValue  && value <= scoreMap.MaxValue)
+                {
+                  return scoreMap.Score;
+                }
+            }
+            string errorMessage = string.Format("Value {0} for {1} is invalid. Please enter a value between {2} and {3}.",
+                 value, measurementType, getMinimumValidValue(scoreMappingList), getMaximumValidValue(scoreMappingList)
+            );
+            throw new InvalidValueException(errorMessage);
+        }
     }
 }
