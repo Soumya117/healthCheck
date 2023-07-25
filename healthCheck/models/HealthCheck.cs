62e2227e864cb50add370f7929584722be5ebb45
Soumya <soumyaupadhyay.6@gmail.com>
2023-07-25 14:02:32 +0200

    Remove unused class

--- healthCheck/models/HealthCheck.cs
+++ healthCheck/models/HealthCheck.cs
@@ -2,14 +2,3 @@ public class HealthCheck
 {
     public List<Measurement> measurements {get;set;}
 }
-
-public class ScoreData 
-{
-    public int Score {get; set;}
-    public string ErrorMessage {get; set;}
-    public ScoreData(int score, string errorMessage)
-    {
-        Score = score;
-        ErrorMessage = errorMessage;
-    }
-}
